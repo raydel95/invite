24eb68c6e144b8b0d241648b6d11e79e5a7e904a
Raydel E. Alonso Baryolo <raydel.alonso@nubank.com.br>
2024-11-07 02:41:27 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@
 })(jQuery);
 
 
-// Set the date we're counting down to
-var endTime = new Date("Nov 29, 2024 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
